e593fac6c738681ddaa4e7596cd7b1db66eb5428
Justin Lee <38745924+plumsily@users.noreply.github.com>
2022-07-11 22:58:15 -0700

    updated api fetch to use async await call

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const App = () => {
   const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [color, setColor] = useState("00FFFFFF");
   const [items, setItems] = useState([]);
   const [numQuotes, setNumQuotes] = useState(0);
@@ -25,25 +25,41 @@ const App = () => {
     document.body.style.background = newColor;
     setColor(newColor);
     setQuote(newQuote);
-    setAuthor(newAuthor);
+    setAuthor(newAuthor !== null ? newAuthor : "unknown");
   };
 
   useEffect(() => {
-    fetch("https://type.fit/api/quotes")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-          setNumQuotes(result.length);
-          getQuotes(result, result.length);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+    const fetchData = async () => {
+      const res = await fetch("https://type.fit/api/quotes");
+      const result = await res.json();
+      setIsLoaded(true);
+      setItems(result);
+      setNumQuotes(result.length);
+      getQuotes(result, result.length);
+    };
+    fetchData().catch((error) => {
+      setIsLoaded(true);
+      setError(error);
+      console.log("fetch failed", error);
+    });
   }, []);
+
+  // useEffect(() => {
+  //   fetch("https://type.fit/api/quotes")
+  //     .then((res) => res.json())
+  //     .then(
+  //       (result) => {
+  //         setIsLoaded(true);
+  //         setItems(result);
+  //         setNumQuotes(result.length);
+  //         getQuotes(result, result.length);
+  //       },
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         setError(error);
+  //       }
+  //     );
+  // }, []);
 
   return (
     <Card
@@ -81,99 +97,5 @@ const App = () => {
     </Card>
   );
 };
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       color: "00FFFFFF",
-//       error: null,
-//       isLoaded: false,
-//       items: [],
-//       numQuotes: 0,
-//       quote: "test",
-//       author: "test",
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch("https://type.fit/api/quotes")
-//       .then((res) => res.json())
-//       .then(
-//         (result) => {
-//           this.setState({
-//             isLoaded: true,
-//             items: result,
-//             numQuotes: result.length,
-//           });
-//           this.getQuotes(result, result.length);
-//         },
-//         (error) => {
-//           this.setState({
-//             isLoaded: true,
-//             error,
-//           });
-//         }
-//       );
-//   }
-
-//   getQuotes = (quotes, number) => {
-//     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-//     const newColor = "#" + randomColor;
-
-//     document.body.style.background = newColor;
-
-//     const randomQuote = Math.floor(Math.random() * number);
-//     const newQuote = quotes[randomQuote].text;
-//     const newAuthor = quotes[randomQuote].author;
-
-//     this.setState({
-//       color: newColor,
-//       quote: newQuote,
-//       author: newAuthor !== null ? newAuthor : "unknown",
-//     });
-//   };
-
-//   render() {
-//     const items = this.state.items;
-//     const numQuotes = this.state.numQuotes;
-
-//     return (
-//       <Card
-//         style={{ width: "500px" }}
-//         id="quote-box"
-//         className="shadow-lg  px-2 py-2"
-//       >
-//         <Card.Body>
-//           <figure>
-//             <blockquote className="blockquote mb-3">
-//               <p id="text">{this.state.quote}</p>
-//             </blockquote>
-//             <figcaption className="blockquote-footer text-end mb-5">
-//               by <cite id="author">{this.state.author}</cite>
-//             </figcaption>
-//           </figure>
-//           <a
-//             id="tweet-quote"
-//             href={`https://twitter.com/intent/tweet?hashtags=quotes&text="${this.state.quote}"%20by%20${this.state.author}`}
-//             className="btn btn-info"
-//             rel="noreferrer"
-//             target="_blank"
-//           >
-//             <FontAwesomeIcon icon={faTwitter} />
-//           </a>
-//           <Button
-//             id="new-quote"
-//             className="float-end"
-//             onClick={() => this.getQuotes(items, numQuotes)}
-//             style={{ background: this.state.color }}
-//           >
-//             Next Quote
-//           </Button>
-//         </Card.Body>
-//       </Card>
-//     );
-//   }
-// }
 
 export default App;
