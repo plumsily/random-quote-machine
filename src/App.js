254e72a886b5517e56d2c88c2b643d2241403302
Justin Lee <38745924+plumsily@users.noreply.github.com>
2022-08-25 16:00:11 -0700

    updated button command to show next quote and added tweet feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Transition } from "@headlessui/react";
 import "./App.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,86 +9,110 @@ const App = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [color, setColor] = useState("00FFFFFF");
-  const [items, setItems] = useState([]);
-  const [numQuotes, setNumQuotes] = useState(0);
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
+  const [data, setData] = useState([]);
+  const [isShowing, setIsShowing] = useState(true);
 
-  const getQuotes = (quotes, number) => {
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    let newColor = "#" + randomColor;
-    let randomQuote = Math.floor(Math.random() * number);
-    let newQuote = quotes[randomQuote].text;
-    let newAuthor = quotes[randomQuote].author;
-
-    document.body.style.background = newColor;
-    setColor(newColor);
-    setQuote(newQuote);
-    setAuthor(newAuthor !== null ? newAuthor : "unknown");
+  const updateQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const result = await response.json();
+      setIsLoaded(true);
+      setData(result);
+    } catch (error) {
+      setIsLoaded(true);
+      setError(error);
+      console.error(error);
+      setData({ content: "Something went wrong" });
+    }
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://type.fit/api/quotes");
-      const result = await res.json();
-      setIsLoaded(true);
-      setItems(result);
-      setNumQuotes(result.length);
-      getQuotes(result, result.length);
-    };
-    fetchData().catch((error) => {
-      setIsLoaded(true);
-      setError(error);
-      console.log("fetch failed", error);
-    });
+    updateQuote();
   }, []);
+
+  useEffect(() => {
+    setColor("#" + Math.floor(Math.random() * 16777215).toString(16));
+  }, [data]);
 
   useEffect(() => {
     //timeout effect?
   });
 
+  const clickTransition = () => {
+    setIsShowing(false);
+    setTimeout(() => {
+      updateQuote();
+    }, 550);
+    setTimeout(() => {
+      setIsShowing(true);
+    }, 650);
+  };
+
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div className="w-screen mx-auto flex flex-column items-center">
-        <div className="text-7xl font-semibold text-white/60 mb-5">
-          "Here is the quote!"
-        </div>
-        <div className="text-2xl font-medium text-white/60">by Justin Lee</div>
+    <div className="flex h-screen justify-center items-center relative">
+      <div className="w-screen px-40 mx-auto flex flex-column items-center">
+        <Transition
+          appear={true}
+          show={isShowing}
+          enter="transition-opacity duration-500 ease-in"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-500 ease-out"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          className="text-6xl font-semibold mb-5 drop-shadow-md text-center leading-snug"
+          style={{ color: color }}
+        >
+          "{data.content}"
+        </Transition>
+        <Transition
+          appear={true}
+          show={isShowing}
+          enter="transition-opacity duration-700 ease-in"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-500 ease-out"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          className="text-3xl font-medium mb-5 drop-shadow-md italic"
+          style={{ color: color }}
+        >
+          - {data.author}
+        </Transition>
+      </div>
+      <div className="absolute bottom-16">
+        <Transition
+          appear={true}
+          show={isShowing}
+          enter="transition-opacity duration-1000 ease-in"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-500 ease-out"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          className="flex flex-row items-center justify-items-center gap-x-3"
+          style={{ color: color }}
+        >
+          <a
+            id="tweet-quote"
+            href={`https://twitter.com/intent/tweet?hashtags=quotes&text="${data.content}"%20by%20${data.author}`}
+            className="w-10 h-10 rounded-xl opacity-80 backdrop-blur flex flex-row items-center justify-center"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <button
+            onClick={() => {
+              clickTransition();
+            }}
+            className="w-32 h-10 rounded-xl opacity-80 backdrop-blur font-medium"
+          >
+            Next Quote
+          </button>
+        </Transition>
       </div>
     </div>
-    // <Card
-    //   style={{ width: "500px" }}
-    //   id="quote-box"
-    //   className="shadow-lg  px-2 py-2"
-    // >
-    //   <Card.Body>
-    //     <figure>
-    //       <blockquote className="blockquote mb-3">
-    //         <p id="text">{quote}</p>
-    //       </blockquote>
-    //       <figcaption className="blockquote-footer text-end mb-5">
-    //         by <cite id="author">{author}</cite>
-    //       </figcaption>
-    //     </figure>
-    //     <a
-    //       id="tweet-quote"
-    //       href={`https://twitter.com/intent/tweet?hashtags=quotes&text="${quote}"%20by%20${author}`}
-    //       className="btn btn-info"
-    //       rel="noreferrer"
-    //       target="_blank"
-    //     >
-    //       <FontAwesomeIcon icon={faTwitter} />
-    //     </a>
-    //     <Button
-    //       id="new-quote"
-    //       className="float-end"
-    //       onClick={() => getQuotes(items, numQuotes)}
-    //       style={{ background: color }}
-    //     >
-    //       Next Quote
-    //     </Button>
-    //   </Card.Body>
-    // </Card>
   );
 };
 
