b161d744e12ba156f5b4550a159f32aa341ece1a
Justin Lee <38745924+plumsily@users.noreply.github.com>
2022-09-01 23:10:40 -0700

    fixed animations pausing and lagging the timer when switching tabs

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ const App = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [isShowing, setIsShowing] = useState(true);
+  const [isVisible, setIsVisible] = useState(true);
+  const [isSwitch, setIsSwitch] = useState(false);
 
   const updateQuote = async () => {
     try {
       const response = await fetch("https://api.quotable.io/random");
       const result = await response.json();
       setIsLoaded(true);
+      setIsVisible(true);
       setData(result);
     } catch (error) {
       setIsLoaded(true);
+      setIsVisible(true);
       setError(error);
       console.error(error);
       setData({ content: "Something went wrong" });
@@ -26,7 +30,27 @@ const App = () => {
 
   useEffect(() => {
     updateQuote();
+    const handleBlur = () => {
+      setIsVisible(false);
+      setIsSwitch(true);
+    };
+    const handleFocus = () => {
+      setIsVisible(true);
+    };
+    window.addEventListener("blur", handleBlur);
+    window.addEventListener("focus", handleFocus);
+    return () => {
+      window.removeEventListener("blur", handleBlur);
+      window.removeEventListener("focus", handleFocus);
+    };
   }, []);
+
+  useEffect(() => {
+    if (isVisible === true && isSwitch === true) {
+      clickTransition();
+      setIsSwitch(false);
+    }
+  }, [isVisible]);
 
   useEffect(() => {
     setColor("#" + Math.floor(Math.random() * 16777215).toString(16));
